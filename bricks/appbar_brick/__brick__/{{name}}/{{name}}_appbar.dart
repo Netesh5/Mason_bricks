5bc21dd797ebb850446b1c3fa6a5ce7f5989da5f
Netesh5 <netesh.paudel@gmail.com>
2023-11-22 11:54:47 +0545

    initial commit

---
+++ bricks/appbar_brick/__brick__/{{name}}/{{name}}_appbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class {{name.pascalCase()}}AppBar extends StatelessWidget implements PreferredSizeWidget {
+  const {{name.pascalCase()}}AppBar({super.key, required this.preferredSize,required this.title});
+
+  @override
+  final Size preferredSize;
+  final String title;
+  
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      elevation: 1,
+    );
+  }
+}
